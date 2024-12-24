02445660797cd3d72fc4b6a082d42cfa76b38860
Svetlana Smirnova <111796441+B4by-Sh4rk@users.noreply.github.com>
2024-12-24 19:25:13 +0300

    1ё

---
+++ script.js
@@ -0,0 +1,61 @@
+function removeTypewriterClass() {
+    const typewriterElements = document.querySelectorAll('.typewriter');
+
+    setTimeout(() => {
+        typewriterElements.forEach(element => {
+            const parent = element.parentNode;
+            if (parent) {
+                while (element.firstChild) {
+                    parent.insertBefore(element.firstChild, element);
+                }
+                parent.removeChild(element);
+            }
+        });
+    }, 2000);
+}
+
+function animateToCenter() {
+    const wrappers = document.querySelectorAll('.wrappers');
+
+    wrappers.forEach(wrapper => {
+        wrapper.classList.add('animateToCenter');
+    });
+
+}
+
+function animateDelText() {
+    const wrappers = document.querySelectorAll('.wrappers');
+
+    wrappers.forEach(wrapper => {
+        wrapper.style.display = 'none';
+    });
+}
+
+function animateLogo() {
+    const logoWrapper = document.querySelector('.wrapper_logo');
+    if (logoWrapper) {
+        logoWrapper.style.opacity = '1';
+    }
+}
+
+function animateAddText() {
+    const main_text = document.querySelector('.main_text');
+    if (main_text) {
+        main_text.classList.add('main_text_visible');
+    }
+    const logoWrapper = document.querySelector('.wrapper_logo');
+    if (logoWrapper) {
+        setTimeout(() => {
+            logoWrapper.classList.add('glitchWrapper_visible');
+        }, 800);
+    }
+}
+
+
+
+removeTypewriterClass();
+
+setTimeout(animateToCenter, 4000);
+setTimeout(animateLogo, 5800);
+setTimeout(animateDelText, 6000);
+setTimeout(animateAddText, 6000);
