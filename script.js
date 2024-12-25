773ad9acf3697616f55c7d853ae1f8253d194396
Svetlana Smirnova <111796441+B4by-Sh4rk@users.noreply.github.com>
2024-12-25 17:50:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,74 @@ function getParameters() {
     let paramString = urlString.split('?')[1];
     let clearString = urlString.split('?')[0];
     let queryString = new URLSearchParams(paramString);
-    const wrappers2 = document.querySelectorAll('.wrappers');
 
-    wrappers2.forEach(wrapper => {
-        wrapper.style.display = 'none';
-    });
+    const wrappers = document.querySelectorAll('.wrappers');
 
-    for (let pair of queryString.entries()) {
-        wrappers2.forEach(wrapper => {
-            wrapper.style.display = 'flex';
+    const hasParams = paramString || localStorage.getItem('hasParams') === 'true';
+
+    if (!hasParams) {
+        wrappers.forEach(wrapper => {
+            wrapper.style.display = 'none';
         });
-    
+
+        function animateLogo() {
+            const logoWrapper = document.querySelector('.wrapper_logo');
+            if (logoWrapper) {
+                logoWrapper.style.opacity = '1';
+            }
+        }
+
+        function animateAddText() {
+            const main_text = document.querySelector('.main_text');
+            if (main_text) {
+                main_text.classList.add('main_text_visible');
+            }
+            const logoWrapper = document.querySelector('.wrapper_logo');
+            const all_wrapp = document.querySelector('.all_wrapp');
+            if (logoWrapper) {
+                setTimeout(() => {
+                    logoWrapper.classList.add('glitchWrapper_visible');
+                }, 100);
+            }
+            if (all_wrapp) {
+                setTimeout(() => {
+                    all_wrapp.classList.add('all_wrapp_show');
+                    setInterval(generateNumbers, 10000);
+                }, 600);
+            }
+        }
+
+        setTimeout(animateLogo);
+        setTimeout(animateAddText, 2000);
+
+        function generateNumbers() {
+            const container = document.querySelector('#all_wrapp');
+            if (!container) {
+                console.error('Элемент с классом .all_wrapp не найден!');
+                return;
+            }
+
+            container.innerHTML = '';
+
+            for (let i = 1; i <= 14; i++) {
+                const numberDiv = document.createElement('div');
+                numberDiv.className = 'number';
+
+                const img = document.createElement('img');
+                img.src = `./numb_${i}.png`;
+                img.alt = `${i}`;
+
+                const randomMarginLeft = Math.floor(Math.random() * (100 - (-20) + 1)) + 0;
+                numberDiv.style.marginLeft = `${randomMarginLeft}vw`;
+
+                numberDiv.appendChild(img);
+                container.appendChild(numberDiv);
+            }
+        }
+        localStorage.removeItem('hasParams');
+    } else {
+        localStorage.setItem('hasParams', 'true');
+
         function removeTypewriterClass() {
             const typewriterElements = document.querySelectorAll('.typewriter');
 
@@ -32,16 +89,13 @@ function getParameters() {
 
         function animateToCenter() {
             const wrappers = document.querySelectorAll('.wrappers');
-
             wrappers.forEach(wrapper => {
                 wrapper.classList.add('animateToCenter');
             });
-
         }
 
         function animateDelText() {
             const wrappers = document.querySelectorAll('.wrappers');
-
             wrappers.forEach(wrapper => {
                 wrapper.style.display = 'none';
             });
@@ -74,15 +128,6 @@ function getParameters() {
             }
         }
 
-
-
-        removeTypewriterClass();
-
-        setTimeout(animateToCenter, 4000);
-        setTimeout(animateLogo, 5800);
-        setTimeout(animateDelText, 6000);
-        setTimeout(animateAddText, 6000);
-
         function generateNumbers() {
             const container = document.querySelector('#all_wrapp');
             if (!container) {
@@ -100,16 +145,22 @@ function getParameters() {
                 img.src = `./numb_${i}.png`;
                 img.alt = `${i}`;
 
-                const randomMarginLeft = Math.floor(Math.random() * (95 - 0 + 1)) + 0;
+                const randomMarginLeft = Math.floor(Math.random() * (100 - (-20) + 1)) + 0;
                 numberDiv.style.marginLeft = `${randomMarginLeft}vw`;
 
                 numberDiv.appendChild(img);
                 container.appendChild(numberDiv);
             }
         }
+
+        removeTypewriterClass();
+        setTimeout(animateToCenter, 4000);
+        setTimeout(animateLogo, 5800);
+        setTimeout(animateDelText, 6000);
+        setTimeout(animateAddText, 6000);
     }
 
     window.history.pushState('', '', clearString);
 }
-getParameters()
 
+getParameters();
